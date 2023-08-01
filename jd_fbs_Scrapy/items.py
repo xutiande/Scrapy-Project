7c16429d58d1af98b02da2a04f27ac4a3860a14b
xutiande <2546582616@qq.com>
2023-08-01 13:44:16 +0800

    creare genspider book

---
+++ jd_fbs_Scrapy/items.py
@@ -0,0 +1,18 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JdFbsScrapyItem(scrapy.Item):
+    # define the fields for your item here like:
+    big_category = scrapy.Field()  # 大分类
+    big_category_link = scrapy.Field()  # 大分类链接
+    small_category = scrapy.Field()  # 小分类
+    small_category_link = scrapy.Field()  # 小分类链接
+    bookname = scrapy.Field()  # 书名
+    author = scrapy.Field()  # 作者
+    link = scrapy.Field()  # 链接
+    price = scrapy.Field()  # 价格
---
+++ jd_fbs_Scrapy/spiders/book.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BookSpider(scrapy.Spider):
+    name = 'book'
+    allowed_domains = ['jd.com']
+    start_urls = ['http://jd.com/']
+
+    def parse(self, response):
+        pass
